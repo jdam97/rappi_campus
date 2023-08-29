2ec8814600bbf414e1c2d73b4b8c1e2e1329f0c4
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-08-29 18:50:30 -0500

    feat: :sparkles: Se agrega el dto de hospedajes

--- Api/V1/routes/hospedajes.routes.js
+++ Api/V1/routes/hospedajes.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import * as HospedajeConsulta from "../controller/HospedajesController.js";
+import { vSchema } from "../helpers/validationSchema.js";
+import { HospedajesSchema } from "../middleware/Hospedajes.js";
 
 const HospedajeApp = Router();
 
 HospedajeApp.get("/",HospedajeConsulta.getAllHospedajes);
 HospedajeApp.get("/:HospedajesID",HospedajeConsulta.getHospedajesById);
-HospedajeApp.post("/",HospedajeConsulta.createHospedajes);
+HospedajeApp.post("/",vSchema(HospedajesSchema),HospedajeConsulta.createHospedajes);
