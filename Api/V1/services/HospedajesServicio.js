23c056a5bee2f2cf325866c1f45ceca17d2191d4
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-08-30 06:22:31 -0500

    refactor: :recycle: Se actualiza el codigo para que la conexion funcione

--- Api/V1/services/HospedajesServicio.js
+++ Api/V1/services/HospedajesServicio.js
@@ -1,21 +1,21 @@
-import { connectDB } from "../database/atlas";
+import { connectDB } from "../database/atlas.js";
 
 export const getAllHospedajes = async ()=>{
-    let db = await connDB();
+    let db = await connectDB();
     let collection = db.collection('hospedajes');
     let data = await collection.find().toArray();
     return data;
 };
 
 export const getHospedajesById = async (HospedajesID)=>{
-    let db = await connDB();
+    let db = await connectDB();
     let collection = db.collection('hospedajes');
     let data = await collection.find({id:HospedajesID}).toArray();
     return data;
 };
 
 export const createHospedajes = async (Hospedajes)=>{
-    let db = await connDB();
+    let db = await connectDB();
     let collection = db.collection('hospedajes');
     let data = await collection.insertOne(Hospedajes);
     return data;
